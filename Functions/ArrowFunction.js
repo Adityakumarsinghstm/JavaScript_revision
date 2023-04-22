ae43f6f3c6c0be62d5a5316a234470d2eccc5d9a
Aditya <adityasingh62065@gmail.com>
2023-04-22 10:35:15 +0530

    arrow function revised

---
+++ Functions/ArrowFunction.js
@@ -0,0 +1,14 @@
+const square = (x) => x * x;
+const sumOfTwoNumber = (x, y) => x + y;
+const display = (x, y) =>
+{
+    console.log(`Adding ${x} and ${y}`)
+    return x+y
+}
+
+const res1 = square(2)
+const res2 = sumOfTwoNumber(34, 23)
+const res3 = display(23,54)
+console.log(res1)
+console.log(res2)
+console.log(res3)
